f8b0d4af1564a8f65d7d4d0f0770c2d71cbec4a5
dang1412 <dttung1412@gmail.com>
2025-10-27 22:33:18 +0700

    feat(server): ws dist

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,28 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const viem_1 = require("viem");
 const chains_1 = require("viem/chains");
-const encode_1 = require("./utils/encode");
+const websocket_1 = require("./websocket");
 function getClientIp(req, ws) {
-    const forwardedFor = req.headers["x-forwarded-for"];
+    const forwardedFor = req.headers['x-forwarded-for'];
     const ip = Array.isArray(forwardedFor)
         ? forwardedFor[0]
-        : forwardedFor?.split(",")[0].trim();
-    return ip || ws._socket.remoteAddress?.replace(/^::ffff:/, "") || "unknown";
+        : forwardedFor?.split(',')[0].trim();
+    return ip || ws._socket.remoteAddress?.replace(/^::ffff:/, '') || 'unknown';
 }
-// ==== CONFIG ====
-// const CONTRACT_ADDRESS = "0x4f25aF152764737E6DDFb8cBEA7be66a553B70F4"
-const CONTRACT_ADDRESS = "0x83514843b0A11398e98e99873908c1d6f1C1CaeA"; // 2025/09/19
-// Event ABI (ví dụ: BoxClaimed)
-const EVENT_ABI = [
-    (0, viem_1.parseAbiItem)("event BoxClaimed(address user, uint16 position, uint16 token)"),
-];
-// wss://base-sepolia.g.alchemy.com/v2/SGhknXwY9r_VlRV44vghO0RfBXc1nhcB
 // Tạo client kết nối RPC qua WebSocket
 const client = (0, viem_1.createPublicClient)({
     chain: chains_1.baseSepolia,
-    transport: (0, viem_1.webSocket)("wss://base-mainnet.g.alchemy.com/v2/SGhknXwY9r_VlRV44vghO0RfBXc1nhcB"),
-    // transport: webSocket("wss://go.getblock.io/d0aca3a299984a3ab6561fbb9cba99af"),
-    // transport: webSocket("wss://base-sepolia.core.chainstack.com/f261d7b1c99ec08c17270535b5ac79b9"),
+    transport: (0, viem_1.webSocket)('wss://base-mainnet.g.alchemy.com/v2/SGhknXwY9r_VlRV44vghO0RfBXc1nhcB'),
+    // transport: webSocket('wss://go.getblock.io/d0aca3a299984a3ab6561fbb9cba99af'),
+    // transport: webSocket('wss://base-sepolia.core.chainstack.com/f261d7b1c99ec08c17270535b5ac79b9'),
     // transport: roundRobinHttp([
     //   // {url: 'https://sepolia.base.org'},
     //   {
@@ -43,36 +35,35 @@ const client = (0, viem_1.createPublicClient)({
 // ==== WEBSOCKET SERVER ====
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 let sockets = [];
-wss.on("connection", (ws, req) => {
+const accountToSockets = {};
+wss.on('connection', (ws, req) => {
     sockets.push(ws);
     const ip = getClientIp(req, ws);
     console.log(`Client connected ${ip}, total:`, sockets.length, new Date());
-    ws.on("close", () => {
+    ws.on('close', () => {
         sockets = sockets.filter((s) => s !== ws);
         console.log(`Client disconnected ${ip}, total:`, sockets.length, new Date());
     });
-});
-// // ==== LISTEN TO CONTRACT EVENTS ====
-const rs = client.watchContractEvent({
-    address: CONTRACT_ADDRESS,
-    abi: EVENT_ABI,
-    eventName: "BoxClaimed",
-    onLogs: (logs) => {
-        console.log("Events received:", logs.length, new Date());
-        for (const log of logs) {
-            console.log(`- User: ${log.args.user}, Position: ${log.args.position}, Token: ${log.args.token}`);
-        }
-        const args = logs.map(log => ({
-            user: '0x', position: log.args?.position || 0, token: log.args?.token || 0
-        }));
-        const encoded = (0, encode_1.encodeBoxClaimedEvents)(args);
-        // const message = JSON.stringify({ type: "BoxClaimed", data: logs })
-        sockets.forEach((ws) => {
-            if (ws.readyState === ws.OPEN) {
-                ws.send(encoded);
+    ws.on('message', (data) => {
+        try {
+            const message = JSON.parse(data.toString());
+            if (message.type === 'subscribe' && message.address) {
+                accountToSockets[message.address] = accountToSockets[message.address] || [];
+                accountToSockets[message.address].push(ws);
+                console.log(`Address ${message.address} registered for WebSocket updates.`);
             }
-        });
-    },
-    pollingInterval: 4000,
+            else if (message.type === 'unsubscribe' && message.address && accountToSockets[message.address]) {
+                accountToSockets[message.address] = accountToSockets[message.address].filter((s) => s !== ws);
+                console.log(`Address ${message.address} unregistered from WebSocket updates.`);
+            }
+        }
+        catch (error) {
+            console.error('Failed to parse WebSocket message:', error);
+        }
+    });
 });
-console.log("✅ WebSocket server running on ws://localhost:8080");
+// claimBox event
+(0, websocket_1.watchClaimBox)(client, sockets);
+// message event
+(0, websocket_1.watchEventMessage)(client, accountToSockets);
+console.log('✅ WebSocket server running on ws://localhost:8080');
